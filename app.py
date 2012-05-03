be605499352013cc609879cc276812f5fc7a4140
Mark Meves <mark.meves@gmail.com>
2012-05-03 05:39:38 -0700

    use jinja templates.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    arr = ['alpha-beta.wav', 'gamma-delta.wav']
-    output = ''
-    for name in arr:
-        output += ("ok: " + name)
-    return output + "Hello World!!"
+    sounds = ['alpha-beta.wav', 'gamma-delta.wav']
+    return render_template('index.html', sounds=sounds) # send tuples
 
 if __name__ == '__main__':
     app.run(debug = True)
