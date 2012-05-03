6c086befbe34d71d46b505f64da781d3cb5b6deb
jesse <jesse@pony.noisebridge.net>
2012-05-03 06:21:17 -0700

    add chime test

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import subprocess
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +10,14 @@ def index():
     sounds = ['alpha-beta.wav', 'gamma-delta.wav']
     return render_template('index.html', sounds=sounds) # send tuples
 
+def test():
+    [ user, password ] = get_cred
+    cmd = [ 'sshpass', '-p', password, 'ssh',
+            '-o', 'StrictHostKeyChecking=no',
+            '-o', 'UserKnownHostsFile=/dev/null',
+            user, 'mpg123 chime.mp3' ]
+    p = subprocess.call(cmd)
+    return 'OK'
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', debug = True)
