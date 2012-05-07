744bd1178088b4c2ccd4a528672dfe1c37fa5c68
Mark Meves <mark.meves@gmail.com>
2012-05-06 22:32:55 -0700

    the list of sounds actually comes from the directory.

--- lib/app/server.py
+++ lib/app/server.py
@@ -3,13 +3,27 @@ from flask import render_template
 from flask import url_for
 import subprocess
 import os
+import app.constant
+
 
 server = Flask(__name__)
 
+# server.sounds_dir = os.path.join(os.path.dirname(__file__), app.constant.SOUNDS_DIR)
+def sounds_dir():
+    return os.path.join(os.path.dirname(__file__), app.constant.SOUNDS_DIR)
+
+def sounds():
+    for dirname, _dirnames, filenames in os.walk(sounds_dir()):
+        _filtered = filter(lambda x: app.constant.SOUND_RE.search(x), filenames)
+        return map(lambda filename: {'label' : filename, 'id' : filename,
+            'path' : os.path.join(dirname, filename)}, _filtered)
+
+
 @server.route('/')
 def index():
-    sounds = ['alpha-beta.wav', 'gamma-delta.wav']
-    return render_template('index.html', sounds=sounds) # send tuples
+    _sounds = sounds()
+    return render_template('index.html', sounds=_sounds, no_sounds=(0 ==
+        len(_sounds)))
 
 def test():
     [ user, password ] = get_cred
